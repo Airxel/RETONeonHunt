3ffe655c2e69739c04418dd6f570f2374ed06449
Airxel <airxeldroide@gmail.com>
2025-02-06 12:13:53 +0000

    Día 1 Mañana 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody bodyRb;
     private Rigidbody playerRb;
     private float playerMovementX;
     private float playerMovementY;
+
+    [SerializeField]
+    public float playerSpeed;
+
+    [SerializeField]
+    public float playerTilt;
+    private float tiltAngle;
 
     private void Start()
     {
@@ -18,7 +27,16 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 playerMovement = new Vector3(playerMovementX, 0.0f, playerMovementY);
 
-        playerRb.AddForce(playerMovement);
+        playerRb.AddForce(playerMovement * playerSpeed);
+
+        Vector3 playerVelocity = playerRb.velocity;
+
+        if (playerVelocity.magnitude > 0.1f)
+        {
+            tiltAngle = Mathf.Clamp(playerVelocity.x * playerTilt, -playerTilt, playerTilt);
+        }
+
+        playerRb.rotation = Quaternion.Euler(0.0f, transform.eulerAngles.y, -tiltAngle);
     }
 
     private void OnMove(InputValue movementValue)
