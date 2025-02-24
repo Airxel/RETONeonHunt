9ea03b1f4cde60790097859288524f0e2c67fe3c
Airxel <airxeldroide@gmail.com>
2025-02-24 13:55:15 +0000

    Día 14 Mañana 2
    Virtual Joystick Try, ...

---
+++ Assets/Scripts/Pruebas/Scripts/UICanvasControllerInput.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class UICanvasControllerInput : MonoBehaviour
+    {
+
+        [Header("Output")]
+        public InputActions inputActions;
+
+        public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        {
+            inputActions.MoveInput(virtualMoveDirection);
+        }
+
+        public void VirtualLookInput(Vector2 virtualLookDirection)
+        {
+            inputActions.LookInput(virtualLookDirection);
+        }
+
+        public void VirtualShootInput(bool virtualShootState)
+        {
+            inputActions.ShootInput(virtualShootState);
+        }
+    }
+
+}
